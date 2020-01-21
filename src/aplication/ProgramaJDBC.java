cf6df132e2c8a39b52237d7040da26d293c13019
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-01-21 19:14:49 -0300

    Classe de entidades do departamento

---
+++ src/aplication/ProgramaJDBC.java
@@ -0,0 +1,13 @@
+package aplication;
+
+import model.entities.Department;
+
+public class ProgramaJDBC {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+	}
+
+}
