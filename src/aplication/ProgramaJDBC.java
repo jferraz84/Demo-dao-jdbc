da3ab7ccce68a2c08f8d1d58ca0b1092337ac119
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-01-21 19:59:29 -0300

    Classe de entidades Vendedor

--- src/aplication/ProgramaJDBC.java
+++ src/aplication/ProgramaJDBC.java
@@ -1,13 +1,20 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class ProgramaJDBC {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller sl = new Seller(21,"Jorge", "jferraz@cpqd.com.br", new Date(), 3000.0, obj);
+		
+		System.out.println(sl);
+		//System.out.println(obj);
 	}
 
 }
