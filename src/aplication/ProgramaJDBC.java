9fabb8e2e371ce9fa23170db9900894103a598b0
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-01-22 12:22:05 -0300

    Classe SellerDaoJDBC  e DaoFactory

--- src/aplication/ProgramaJDBC.java
+++ src/aplication/ProgramaJDBC.java
@@ -2,6 +2,8 @@ package aplication;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class ProgramaJDBC {
 		Department obj = new Department(1, "Books");
 		
 		Seller sl = new Seller(21,"Jorge", "jferraz@cpqd.com.br", new Date(), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(sl);
 		//System.out.println(obj);
